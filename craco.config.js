73dbd5299db9c585e9912ea9d2c339511db58c31
angrypie <cookies.in.chocolate@gmail.com>
2019-04-03 10:25:36 +0300

    Add webpack aliases to avoid relative path ../../../../hell

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,32 @@
-module.exports = {
-	babel: {
-		plugins: [
-			['@babel/plugin-proposal-decorators', { legacy: true }],
-			['styled-jsx/babel'],
-		]
+const path = require('path')
+
+const babel = {
+	plugins: [
+		['@babel/plugin-proposal-decorators', { legacy: true }],
+		['styled-jsx/babel'],
+	]
+}
+
+const webpack = {
+	resolve: {
+		alias: {
+			...createAliases(['resources', 'components', 'routes', 'layouts', 'store']),
+		}
 	}
+}
+
+module.exports = {
+	babel,
+	webpack: {
+		configure: webpack,
+	},
+}
+
+// WEBPACK HELPER FUNCTIONS
+
+function createAliases(aliases) {
+	return aliases.reduce((ac, cur) => {
+		ac[cur] = path.resolve(__dirname, `src/${cur}/`)
+		return ac
+	}, {})
 }
